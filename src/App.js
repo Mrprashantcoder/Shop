8fa6e4c7206b244eb069fbd7b28104b4712c1a6e
Prashant <prashantcoder70@gmail.com>
2024-07-19 00:00:21 +0530

    all

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ const App = ()=>
 {
   return(
     <>
+      <h1 style={{textAlign:"center",padding:"15px"}}>PRS Shoping</h1>
         <div className={Style.con}>
           <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+         
+         
+          
         </div>
     </>
   )
